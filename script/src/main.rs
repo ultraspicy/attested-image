c307458cc7f8876752bc3b3ac25e944ae718fef8
ultraspicy <element.no.88@gmail.com>
2024-03-24 11:12:43 -0700

    Migrate the close PR from sp1 to this repo

--- script/src/main.rs
+++ script/src/main.rs
@@ -1,22 +1,23 @@
 //! A simple script to generate and verify the proof of a given program.
 
-use sp1_core::{SP1Prover, SP1Stdin, SP1Verifier};
+use sp1_core::{utils, SP1Prover, SP1Stdin, SP1Verifier};
 
 const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
-    // Generate proof.
+    utils::setup_tracer();
+
+    let input: &mut [u8] = &mut [3, 1, 2, 3, 1, 2, 4];
+
     let mut stdin = SP1Stdin::new();
-    let n = 186u32;
-    stdin.write(&n);
+    stdin.write_slice(input);
+
     let mut proof = SP1Prover::prove(ELF, stdin).expect("proving failed");
 
     // Read output.
-    let a = proof.stdout.read::<u128>();
-    let b = proof.stdout.read::<u128>();
-    println!("a: {}", a);
-    println!("b: {}", b);
+    let ret = proof.stdout.read::<u8>();
 
+    println!("ret: {}", ret);
     // Verify proof.
     SP1Verifier::verify(ELF, &proof).expect("verification failed");
 
@@ -25,5 +26,5 @@ fn main() {
         .save("proof-with-io.json")
         .expect("saving proof failed");
 
-    println!("successfully generated and verified proof for the program!")
+    println!("succesfully generated and verified proof for the program!")
 }
