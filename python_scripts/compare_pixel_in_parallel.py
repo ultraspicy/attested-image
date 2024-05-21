80a840c7d9b38e708e2277dab621124626b3ceaf
ultraspicy <element.no.88@gmail.com>
2024-05-20 22:41:52 -0700

    Add deleted function part1, convert_rgb_jsons_to_circom_input

--- python_scripts/compare_pixel_in_parallel.py
+++ python_scripts/compare_pixel_in_parallel.py
@@ -35,3 +35,34 @@ thread to process R, G, B channel respectively to generate a series of proof
 The first step is convert the RGB json into the format that will be used in ZKP later
 The second step is to run the ZKP and collect all proof
 '''
+import json 
+
+def convert_rgb_jsons_to_circom_input(rgb_json_path1, rgb_json_path2, output_json_path):
+    # Read the first RGB JSON file
+    with open(rgb_json_path1, 'r') as file:
+        rgb_data1 = json.load(file)
+    
+    # Read the second RGB JSON file
+    with open(rgb_json_path2, 'r') as file:
+        rgb_data2 = json.load(file)
+    
+    # Prepare the new structure for Circom input
+    circom_input = {"in1": [], "in2": []}
+    
+    # Convert the first RGB data to Circom's expected format and add to in1
+    for pixel in rgb_data1:
+        circom_input["in1"].append(str(pixel['r']))
+        circom_input["in1"].append(str(pixel['g']))
+        circom_input["in1"].append(str(pixel['b']))
+    
+    # Convert the second RGB data to Circom's expected format and add to in2
+    for pixel in rgb_data2:
+        circom_input["in2"].append(str(pixel['r']))
+        circom_input["in2"].append(str(pixel['g']))
+        circom_input["in2"].append(str(pixel['b']))
+
+    # Write the converted data to a new JSON file
+    with open(output_json_path, 'w') as file:
+        json.dump(circom_input, file, indent=4)
+    
+    print(f"Converted JSON saved to {output_json_path}")
