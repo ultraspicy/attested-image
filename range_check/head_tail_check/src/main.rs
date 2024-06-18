c3d04d508453dc21d3c4b89f000d0f2d435c6767
ultraspicy <element.no.88@gmail.com>
2024-06-17 23:17:12 -0700

    Change the implementation of range check

--- range_check/head_tail_check/src/main.rs
+++ range_check/head_tail_check/src/main.rs
@@ -21,38 +21,21 @@ fn main() -> Result<()> {
     let config: CircuitConfig = CircuitConfig::standard_recursion_config();
     let mut builder = CircuitBuilder::<F, D>::new(config);
 
-    let vec: Vec<u64> = util::read_vector_from_file("resources/vec1.txt")?;
-    let vec_field: Vec<F> = vec.iter()
-                                                 .map(|&x| F::from_canonical_u64(x))
-                                                 .collect();
+    let range = builder.add_virtual_target();
 
-    //
-    // STEP2: build the circuit by adding constraints of checking the head and tail are the exactly as expected
-    // The public statement is "I have a vector, that aftering sorted, the head is X, and tail is Y "
-    // The secret witness is the vecs, which generate the proof and are hidden from public
-    //
+    // load data from file and add them onto Goldilocks field
+    let vec: Vec<i64> = util::read_vector_from_file("resources/vec1.txt")?;
+    let vec_field: Vec<F> = vec.iter().map(|&x| {
+        if x >= 0 {
+            F::from_canonical_u64(x as u64)
+        } else {
+            let field_modulus = (1u64 << 64) - (1u64 << 32) + 1;
+            F::from_canonical_u64((field_modulus as i64 + x) as u64)
+        }
+    }).collect();
 
-    let upper = F::from_canonical_u64(10);
-    let lower = upper.clone() * F::NEG_ONE;
+    builder.range_check(value, log_max);
 
-    // Convert bounds into targets
-    let lower_bound_target = builder.constant(lower);
-    let upper_bound_target = builder.constant(upper);
-
-    // Convert field elements to a type that supports sorting
-    let mut vec_sorted: Vec<u64> = vec_field.iter().map(|&x| x.to_canonical_u64()).collect();
-    vec_sorted.sort();
-
-    // Convert sorted elements back to field elements
-    let sorted_vec_field: Vec<F> = vec_sorted.iter().map(|&x| F::from_canonical_u64(x)).collect();
-    // Convert sorted elements to Targets
-    let sorted_vec_targets: Vec<Target> = sorted_vec_field.iter().map(|&x| builder.constant(x)).collect();
-
-    let head = sorted_vec_targets[0];
-    let tail = sorted_vec_targets[sorted_vec_field.len() - 1];
-
-    builder.connect(lower_bound_target, head);
-    builder.connect(upper_bound_target, tail);
     
 
     // boilerplate code for benchmark, prove and verify
--- range_check/head_tail_check/src/util.rs
+++ range_check/head_tail_check/src/util.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn read_vector_from_file(filename: &str) -> io::Result<Vec<u64>> {
+pub fn read_vector_from_file(filename: &str) -> io::Result<Vec<i64>> {
     let path = Path::new(filename);
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
@@ -11,7 +11,7 @@ pub fn read_vector_from_file(filename: &str) -> io::Result<Vec<u64>> {
     for line in reader.lines() {
         let line = line?;
         for value in line.split(',') {
-            if let Ok(num) = value.trim().parse::<u64>() {
+            if let Ok(num) = value.trim().parse::<i64>() {
                 vector.push(num);
             }
         }
