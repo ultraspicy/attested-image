2a52cdc90d2464dd0dd905a0a09d3a4f8e28c12d
ultraspicy <element.no.88@gmail.com>
2024-06-20 22:41:01 -0700

    Minor change on polynomial permutation check to accept file input

--- range_check/polynomial_permutation_check/src/main.rs
+++ range_check/polynomial_permutation_check/src/main.rs
@@ -1,3 +1,5 @@
+mod util;
+
 use anyhow::Result;
 use plonky2::field::goldilocks_field::GoldilocksField;
 use plonky2::field::types::Field;
@@ -7,12 +9,11 @@ type F = GoldilocksField;
 
 fn main() -> Result<()> {
     // Example sets Ω and their mappings f and g
-    let omega = vec![1, 2, 3];
-    let f_omega = vec![4, 5, 6]; // f(Ω)
-    let g_omega = vec![6, 5, 4]; // g(Ω), a permutation of f(Ω)
+    let f_omega = util::read_vector_from_file("resources/gen_10.txt")?;
+    let g_omega = util::read_vector_from_file("resources/gen_10.txt")?;
 
     // Construct the polynomials and perform the permutation check
-    let permutation_check = check_permutation(&omega, &f_omega, &g_omega)?;
+    let permutation_check = check_permutation(&f_omega, &g_omega)?;
 
     if permutation_check {
         println!("f(Ω) is a permutation of g(Ω)");
@@ -38,13 +39,13 @@ fn construct_polynomial(values: &[F]) -> Vec<F> {
     poly
 }
 
-/// Evaluate the polynomial at a given point using Horner's method
-fn evaluate_polynomial(poly: &[F], x: F) -> F {
+/// open the polynomial at a given point using Horner's method
+fn open_polynomial(poly: &[F], x: F) -> F {
     poly.iter().rev().fold(F::ZERO, |acc, &coeff| acc * x + coeff)
 }
 
 /// Check if f(Ω) is a permutation of g(Ω)
-fn check_permutation(omega: &[u32], f_omega: &[u32], g_omega: &[u32]) -> Result<bool> {
+fn check_permutation(f_omega: &[usize], g_omega: &[usize]) -> Result<bool> {
     // Convert to field elements
     let f_values: Vec<F> = f_omega.iter().map(|&x| F::from_canonical_usize(x as usize)).collect();
     let g_values: Vec<F> = g_omega.iter().map(|&x| F::from_canonical_usize(x as usize)).collect();
@@ -60,9 +61,9 @@ fn check_permutation(omega: &[u32], f_omega: &[u32], g_omega: &[u32]) -> Result<
         F::from_canonical_usize(rng.gen_range(0..100))
     };
 
-    // Evaluate the polynomials at r
-    let f_r = evaluate_polynomial(&f_poly, r);
-    let g_r = evaluate_polynomial(&g_poly, r);
+    // open the polynomials at a random r
+    let f_r = open_polynomial(&f_poly, r);
+    let g_r = open_polynomial(&g_poly, r);
 
     // Perform the product check
     let prod_check = f_r / g_r;
