831aaf0c8be2fafc60224e8f782877044df358ba
ultraspicy <element.no.88@gmail.com>
2024-06-20 19:42:57 -0700

    Add circuit to benchmark a native range check gadget

---
+++ range_check/native_range_check_benchmark/src/main.rs
@@ -0,0 +1,59 @@
+mod util;
+
+use anyhow::Result;
+use plonky2::field::types::Field;
+use plonky2::iop::witness::{PartialWitness, WitnessWrite};
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2::plonk::circuit_data::CircuitConfig;
+use plonky2::plonk::config::{GenericConfig, PoseidonGoldilocksConfig};
+use std::time::Instant;
+
+/// An example of using Plonky2 to prove that given values lie in a given range.
+fn main() -> Result<()> {
+    const D: usize = 2;
+    type C = PoseidonGoldilocksConfig;
+    type F = <C as GenericConfig<D>>::F;
+
+    let config = CircuitConfig::standard_recursion_config();
+    let mut builder = CircuitBuilder::<F, D>::new(config);
+
+    // The list of secret values.
+    let values: Vec<usize> = util::read_vector_from_file("resources/vec1.txt")?;
+
+    let log_max = 6;
+    let mut targets = Vec::new();
+
+    for &val in &values {
+        let target = builder.add_virtual_target();
+        builder.register_public_input(target);
+        builder.range_check(target, log_max);
+        targets.push((target, val));
+    }
+
+    let mut pw = PartialWitness::new();
+    for &(target, val) in &targets {
+        pw.set_target(target, F::from_canonical_usize(val));
+    }
+
+    // Boilerplate code for benchmark, prove and verify
+    let start_build = Instant::now();
+    let data = builder.build::<C>();
+    let build_duration = start_build.elapsed();
+    println!("Circuit built in: {:?}", build_duration);
+
+    let start_prove = Instant::now();
+    let proof = data.prove(pw)?;
+    let prove_duration = start_prove.elapsed();
+    println!("Proof generated in: {:?}", prove_duration);
+
+    println!("Permutation proof generated successfully.");
+
+    let start_verify = Instant::now();
+    data.verify(proof)?;
+    let verify_duration = start_verify.elapsed();
+    println!("Proof verified in: {:?}", verify_duration);
+
+    println!("Proof verified successfully.");
+
+    Ok(())
+}
---
+++ range_check/native_range_check_benchmark/src/util.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub fn read_vector_from_file(filename: &str) -> io::Result<Vec<usize>> {
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+    let reader = io::BufReader::new(file);
+    
+    let mut vector = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        for value in line.split(',') {
+            if let Ok(num) = value.trim().parse::<usize>() {
+                vector.push(num);
+            }
+        }
+    }
+    
+    Ok(vector)
+}
