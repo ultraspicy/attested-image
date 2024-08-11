cd71ac5a8ac33bf1caf859992676d7d2490dafad
ultraspicy <element.no.88@gmail.com>
2024-08-10 20:05:03 -0700

    chore: minor changes

--- resizing_video/resize_image.c
+++ resizing_video/resize_image.c
@@ -12,6 +12,9 @@
 #define SWS_BILINEAR          2
 #define SWS_BICUBIC           4
 
+#define FILTER_BITS 16 // fix point arithmetic
+#define FILTER_SCALE (1 << FILTER_BITS)
+
 typedef struct {
     int *filterPos;
     int16_t *filter;
@@ -19,38 +22,19 @@ typedef struct {
     int *xInc;
 } SwsContext;
 
-/*
-*   convert a 8 bit color depth to 15 bit 
-*/
-// static void hScale8To15(int16_t *dst, int dstW,
-//                           const uint8_t *src, const int16_t *filter,
-//                           const int32_t *filterPos, int filterSize) {
-//     int i;
-//     for (i = 0; i < dstW; i++) {
-//         int j;
-//         int srcPos = filterPos[i];
-//         int val    = 0;
-//         for (j = 0; j < filterSize; j++) {
-//             val += ((int)src[srcPos + j]) * filter[filterSize * i + j];
-//         }
-//         dst[i] = FFMIN(val >> 7, (1 << 15) - 1); // the cubic equation does overflow ...
-//     }
-// }
-
-
 typedef struct {
     int *filterPos;
     int16_t *filter;
     int filterSize;
     int *xInc;
-} CustomSwsContext;
+} Context;
 
-static CustomSwsContext* custom_sws_alloc_context() {
-    CustomSwsContext *c = (CustomSwsContext*)calloc(1, sizeof(CustomSwsContext));
+static Context* alloc_context() {
+    Context *c = (Context*)calloc(1, sizeof(Context));
     return c;
 }
 
-static void custom_sws_freeContext(CustomSwsContext *c) {
+static void free_context(Context *c) {
     if (c) {
         free(c->filterPos);
         free(c->filter);
@@ -59,9 +43,9 @@ static void custom_sws_freeContext(CustomSwsContext *c) {
     }
 }
 
-static int custom_initFilter(CustomSwsContext *c, int srcW, int dstW, int filterSize) {
+static int init_filter(Context *c, int srcW, int dstW, int filterSize) {
     int i, j;
-    int fone = 1 << 14;
+    int fone = 1 << FILTER_BITS;
     c->filterPos = (int*)malloc(dstW * sizeof(int));
     c->filter = (int16_t*)malloc(dstW * filterSize * sizeof(int16_t));
     c->filterSize = filterSize;
@@ -69,28 +53,34 @@ static int custom_initFilter(CustomSwsContext *c, int srcW, int dstW, int filter
     if (!c->filterPos || !c->filter)
         return -1;
 
-    double scale = (double)srcW / dstW;
+    int64_t xInc = (((int64_t)srcW << 16) / dstW + 1) >> 1;
 
     for (i = 0; i < dstW; i++) {
-        double center = (i + 0.5) * scale - 0.5;
-        int left = (int)center;
-        c->filterPos[i] = left;
+        int64_t srcPos = ((int64_t)i * xInc) >> 16;
+        int64_t xxInc = xInc & 0xffff;
+        int xx = xxInc * (1 << 14) / xInc;
+
+        c->filterPos[i] = srcPos;
 
         for (j = 0; j < filterSize; j++) {
-            double d = fabs(center - (left + j));
-            int coeff = (int)((1.0 - d) * fone + 0.5);
-            if (coeff < 0) coeff = 0;
-            c->filter[i * filterSize + j] = coeff;
+            int64_t coeff;
+            if (j == 0) {
+                coeff = (1 << 30) - xx * (1 << 14);
+            } else {
+                coeff = xx * (1 << 14);
+            }
+            c->filter[i * filterSize + j] = (int32_t)((coeff + (1 << 15)) >> 16);
         }
 
-        int sum = 0;
+        // Normalize filter coefficients
+        int64_t sum = 0;
         for (j = 0; j < filterSize; j++)
             sum += c->filter[i * filterSize + j];
         
-        if (sum != fone) {
+        if (sum != FILTER_SCALE) {
             for (j = 0; j < filterSize; j++) {
-                int corr = ((int64_t)c->filter[i * filterSize + j] * fone) / sum;
-                c->filter[i * filterSize + j] = (int16_t)corr;
+                int64_t coeff = (int64_t)c->filter[i * filterSize + j] * FILTER_SCALE / sum;
+                c->filter[i * filterSize + j] = (int32_t)coeff;
             }
         }
     }
@@ -101,37 +91,38 @@ static int custom_initFilter(CustomSwsContext *c, int srcW, int dstW, int filter
 void resize_image(const uint8_t* input, uint8_t* output, 
                   int input_width, int input_height, 
                   int output_width, int output_height) {
-    CustomSwsContext *c = custom_sws_alloc_context();
+    SwsContext *c = custom_sws_alloc_context();
     int filterSize = 2;  // for bilinear
     
     if (custom_initFilter(c, input_width, output_width, filterSize) < 0) {
-        custom_sws_freeContext(c);
+        custom_sws_alloc_context(c);
         return;
     }
 
     double scale_y = (double)input_height / output_height;
 
     for (int i = 0; i < output_height; i++) {
-        double sy = i * scale_y;
-        int sy_floor = (int)sy;
-        int vy = (int)((sy - sy_floor) * (1 << 14));
+        int srcY = (int)(i * scale_y);
+        int nextY = FFMIN(srcY + 1, input_height - 1);
+        int wy = (int)((i * scale_y - srcY) * FILTER_SCALE + 0.5);
 
         for (int j = 0; j < output_width; j++) {
-            int sx = c->filterPos[j];
+            int srcX = c->filterPos[j];
             int16_t *filter = &c->filter[j * filterSize];
 
-            int val = 0;
-            for (int y = 0; y < 2; y++) {
-                int src_y = FFMIN(sy_floor + y, input_height - 1);
-                int vy_weight = y == 0 ? (1 << 14) - vy : vy;
-                for (int x = 0; x < 2; x++) {
-                    int src_x = FFMIN(sx + x, input_width - 1);
-                    int pos = src_y * input_width + src_x;
-                    val += input[pos] * filter[x] * vy_weight;
-                }
+            int32_t val = 0;
+            for (int k = 0; k < filterSize; k++) {
+                int x = FFMIN(srcX + k, input_width - 1);
+                int coeff_x = filter[k];
+                
+                int pixel1 = input[srcY * input_width + x];
+                int pixel2 = input[nextY * input_width + x];
+                
+                int interpolated = (pixel1 * (FILTER_SCALE - wy) + pixel2 * wy) >> FILTER_BITS;
+                val += interpolated * coeff_x;
             }
 
-            val = (val + (1 << 27)) >> 28;
+            val = (val + (1 << (FILTER_BITS - 1))) >> FILTER_BITS;
             output[i * output_width + j] = FFMIN(FFMAX(val, 0), 255);
         }
     }
